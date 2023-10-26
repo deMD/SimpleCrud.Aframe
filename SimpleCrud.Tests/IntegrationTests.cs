a71b256e731aaad3d29706b90c22ceb26dd94f90
d.demoel <d.demoel@wearetriple.co>
2023-10-26 13:29:10 +0200

    Added verify

--- SimpleCrud.Tests/IntegrationTests.cs
+++ SimpleCrud.Tests/IntegrationTests.cs
@@ -2,6 +2,7 @@
 
 namespace SimpleCrud.Tests;
 
+[UsesVerify]
 public sealed class IntegrationTests : IClassFixture<SimpleCrudApplicationFactory>
 {
     private readonly SimpleCrudApplicationFactory _webApplicationFactory;
@@ -18,5 +19,7 @@ public sealed class IntegrationTests : IClassFixture<SimpleCrudApplicationFactor
         var response = await client.PostAsync("crud", null);
 
         response.Should().BeSuccessful();
+        var content = await response.Content.ReadAsStringAsync();
+        await Verify(content);
     }
 }
